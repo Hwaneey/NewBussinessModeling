5d4807ed9ab03506fb4e236314aedc96e0880fe5
bomi98 <57211669+bomi98@users.noreply.github.com>
2020-05-07 21:20:24 +0900

    Add files via upload

---
+++ UClair/src/analyzer/ui/virtual/VirtualTagAnalyzeResultTableModel.java
@@ -0,0 +1,148 @@
+package analyzer.ui.virtual;
+
+import java.util.List;
+
+
+import javax.swing.table.AbstractTableModel;
+
+import analyzer.analysis.virtual.VirtualDepend;
+import analyzer.constants.AnalyzerConstants;
+import com.naru.uclair.tag.DataTag;
+import com.naru.uclair.tag.ITagConstants;
+import com.naru.uclair.tag.ItemDeviceAdapter;
+import com.naru.uclair.tag.MapDeviceAdapter;
+
+/**
+ *
+ * <pre>
+ * NAME   : com.naru.uclair.analyzer.ui.virtual.VirtualTagAnalyzeResultTableModel.java
+ * DESC   : 가상태그 종속성 분석 결과 표시 TableModel 클래스.
+ *
+ * references : 설계서 NARU-XXX-XXX-XXX
+ *
+ * Copyright 2012 NARU Technology All rights reserved
+ * <pre>
+ *
+ * @author US Laboratory naruteclab4
+ * @since 2012. 6. 21.
+ * @version 1.0
+ *
+ */
+public class VirtualTagAnalyzeResultTableModel extends AbstractTableModel {
+
+	/**
+	 * 0: 태그명
+	 * 1: 노드명
+	 * 2: 데이터 형식
+	 * 3: 장비명
+	 * 4: 영역
+	 */
+	private static final long serialVersionUID = 1L;
+	private String[] columnNames = {AnalyzerConstants.getString("VirtualTagAnalyzeResultTableModel.ColumnName.Event"),
+			AnalyzerConstants.getString("VirtualTagAnalyzeResultTableModel.ColumnName.Tag"), //$NON-NLS-1$
+			AnalyzerConstants.getString("VirtualTagAnalyzeResultTableModel.ColumnName.Node"), //$NON-NLS-1$
+			AnalyzerConstants.getString("VirtualTagAnalyzeResultTableModel.ColumnName.DataType"), //$NON-NLS-1$
+			AnalyzerConstants.getString("VirtualTagAnalyzeResultTableModel.ColumnName.Device"), //$NON-NLS-1$
+			AnalyzerConstants.getString("VirtualTagAnalyzeResultTableModel.ColumnName.Area")}; //$NON-NLS-1$
+	private List<VirtualDepend> tagList;
+
+	/* (non-Javadoc)
+	 * @see javax.swing.table.TableModel#getRowCount()
+	 */
+	@Override
+	public int getRowCount() {
+		if(null != tagList) {
+			return tagList.size();
+		}
+		return 0;
+	}
+	
+	@Override
+	public String getColumnName(int column) {
+		return columnNames[column];
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.swing.table.TableModel#getColumnCount()
+	 */
+	@Override
+	public int getColumnCount() {
+		return columnNames.length;
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.swing.table.TableModel#getValueAt(int, int)
+	 */
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		Object rowData = null;
+		if( null != tagList) {
+			if(getRowCount()>rowIndex){
+				switch(columnIndex){
+				case 0:
+					rowData = tagList.get(rowIndex).getEventName();
+					break;
+				case 1:
+					rowData = tagList.get(rowIndex).getTag().getKey();
+					break;
+				case 2:
+					rowData = tagList.get(rowIndex).getTag().getNode(); 
+					break;
+				case 3:
+					rowData = getDataType(tagList.get(rowIndex).getTag().getDataType());
+					break;
+				case 4:
+					if(tagList.get(rowIndex).getTag() instanceof DataTag) {
+						rowData = ((DataTag)tagList.get(rowIndex).getTag()).getDeviceAdapter().getDeviceName();
+					}
+					break;
+				case 5:
+					if(tagList.get(rowIndex).getTag() instanceof DataTag) {
+						if(((DataTag)tagList.get(rowIndex).getTag()).getDeviceAdapter() instanceof MapDeviceAdapter) {
+							rowData = String.valueOf(((MapDeviceAdapter)((DataTag)tagList.get(rowIndex).getTag()).getDeviceAdapter()).getArea());
+						}
+						if(((DataTag)tagList.get(rowIndex).getTag()).getDeviceAdapter() instanceof ItemDeviceAdapter) {
+							rowData = ((ItemDeviceAdapter)((DataTag)tagList.get(rowIndex).getTag()).getDeviceAdapter()).getItemName();
+						}
+					}
+					break;
+				}
+			}
+			return rowData;
+			
+		}
+		return null;
+	}
+	
+	private String getDataType(int type) {
+		String returnString = null;
+		switch (type) {
+		case ITagConstants.TAG_DATATYPE_DIGITAL:
+			returnString = "DIGITAL";
+			break;
+		case ITagConstants.TAG_DATATYPE_INTEGER:
+			returnString = "INTEGER";
+			break;
+		case ITagConstants.TAG_DATATYPE_REAL:
+			returnString = "REAL";
+			break;
+		case ITagConstants.TAG_DATATYPE_STRING:
+			returnString = "STRING";
+			break;
+		}
+		return returnString;
+	}
+
+//	public void setVirtualTagDepends(List<Tag> tagList) {
+//		this.tagList =tagList;
+//		
+//		fireTableDataChanged();
+//	}
+	
+	public void setVirtualDepends(List<VirtualDepend> tagList) {
+		this.tagList =tagList;
+		
+		fireTableDataChanged();
+	}
+
+}
---
+++ UClair/src/analyzer/ui/virtual/VirtualTagAnalyzeResultView.java
@@ -0,0 +1,123 @@
+package analyzer.ui.virtual;
+
+import java.awt.GridBagConstraints;
+
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.util.List;
+
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+import com.jidesoft.document.DocumentComponent;
+import com.jidesoft.document.DocumentPane;
+
+import analyzer.constants.AnalyzerConstants;
+import analyzer.frame.AnalyzerMainFrame;
+import analyzer.analysis.virtual.VirtualDepend;
+
+
+/************************************************
+ * @date	: 2020. 5.07.
+ * @책임자 : 박보미
+ * @설명  	: Virtual 태그 종속성 분석 결과 창 클래스
+ * @변경이력 	: 
+ ************************************************/
+
+public class VirtualTagAnalyzeResultView extends JPanel {
+
+	/**
+	 * ��ü ����ȭ ���� ���̵�.
+	 */
+	private static final long serialVersionUID = 1L;
+	private JTable resultTable;
+	private VirtualTagAnalyzeResultTableModel tableModel;
+	private JLabel tagNameLabel;
+	
+	private static DocumentPane _workspacePane =  AnalyzerMainFrame._workspacePane;
+
+	/**
+	 * Create the panel.
+	 */
+	public VirtualTagAnalyzeResultView() {
+		
+		initializeUi();
+		final DocumentComponent document = new DocumentComponent(initializeUi(), "�������� �ʴ� �±� �м�");
+        _workspacePane.openDocument(document);
+		
+	}
+	
+	private JTable initializeUi() {
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+		
+		
+		JLabel tagTitleLabel = new JLabel(AnalyzerConstants.getString("VirtualTagAnalyzeResultView.Label.SelectedTag")); //$NON-NLS-1$
+		GridBagConstraints gbc_tagTitleLabel = new GridBagConstraints();
+		gbc_tagTitleLabel.anchor = GridBagConstraints.WEST;
+		gbc_tagTitleLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_tagTitleLabel.gridx = 0;
+		gbc_tagTitleLabel.gridy = 0;
+		add(tagTitleLabel, gbc_tagTitleLabel);
+		
+		JLabel lineLabel = new JLabel("-----"); //$NON-NLS-1$
+		GridBagConstraints gbc_lineLabel = new GridBagConstraints();
+		gbc_lineLabel.anchor = GridBagConstraints.WEST;
+		gbc_lineLabel.insets = new Insets(0, 0, 5, 0);
+		gbc_lineLabel.gridx = 1;
+		gbc_lineLabel.gridy = 0;
+		add(lineLabel, gbc_lineLabel);
+		
+		tagNameLabel = new JLabel(AnalyzerConstants.getString("VirtualTagAnalyzeResultView.Label.TagName")); //$NON-NLS-1$
+		GridBagConstraints gbc_tagNameLabel = new GridBagConstraints();
+		gbc_tagNameLabel.fill = GridBagConstraints.HORIZONTAL;
+		gbc_tagNameLabel.gridwidth = 3;
+		gbc_tagNameLabel.insets = new Insets(0, 10, 10, 10);
+		gbc_tagNameLabel.gridx = 0;
+		gbc_tagNameLabel.gridy = 1;
+		add(tagNameLabel, gbc_tagNameLabel);
+		
+		JLabel resultTitleLabel = new JLabel(AnalyzerConstants.getString("VirtualTagAnalyzeResultView.Label.AnalyzeResult")); //$NON-NLS-1$
+		GridBagConstraints gbc_resultTitleLabel = new GridBagConstraints();
+		gbc_resultTitleLabel.anchor = GridBagConstraints.WEST;
+		gbc_resultTitleLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_resultTitleLabel.gridx = 0;
+		gbc_resultTitleLabel.gridy = 2;
+		add(resultTitleLabel, gbc_resultTitleLabel);
+		
+		JLabel lineLabel2 = new JLabel("-----"); //$NON-NLS-1$
+		GridBagConstraints gbc_lineLabel2 = new GridBagConstraints();
+		gbc_lineLabel2.anchor = GridBagConstraints.WEST;
+		gbc_lineLabel2.insets = new Insets(0, 0, 5, 0);
+		gbc_lineLabel2.gridx = 1;
+		gbc_lineLabel2.gridy = 2;
+		add(lineLabel2, gbc_lineLabel2);
+		
+		JScrollPane resultTableScrollPane = new JScrollPane();
+		GridBagConstraints gbc_resultTableScrollPane = new GridBagConstraints();
+		gbc_resultTableScrollPane.gridwidth = 3;
+		gbc_resultTableScrollPane.fill = GridBagConstraints.BOTH;
+		gbc_resultTableScrollPane.gridx = 0;
+		gbc_resultTableScrollPane.gridy = 3;
+		add(resultTableScrollPane, gbc_resultTableScrollPane);
+		
+		tableModel = new VirtualTagAnalyzeResultTableModel();
+		resultTable = new JTable(tableModel);
+		resultTableScrollPane.setViewportView(resultTable);
+		
+		return resultTable;
+	}
+	
+	public void setVirtualTagDepends(List<VirtualDepend> tagList, String tagKey) {
+		tagNameLabel.setText(tagKey);
+		tableModel.setVirtualDepends(tagList);
+	}
+
+}
