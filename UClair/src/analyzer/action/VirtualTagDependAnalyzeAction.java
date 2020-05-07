e87fd5ee1bc7c218d5ae6a0882dff647621c1f9f
bomi98 <57211669+bomi98@users.noreply.github.com>
2020-05-07 21:22:00 +0900

    Add files via upload

--- UClair/src/analyzer/action/VirtualTagDependAnalyzeAction.java
+++ UClair/src/analyzer/action/VirtualTagDependAnalyzeAction.java
@@ -4,32 +4,90 @@ import java.awt.event.ActionEvent;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
+import javax.swing.JOptionPane;
+
+import com.naru.common.ui.NormalProgressWindow;
+import analyzer.action.AbstractProjectRelatedAction;
+import analyzer.ui.virtual.VirtualTagAnalyzeDialog;
+
+import analyzer.analysis.dangling.DanglingTagAnalyzer;
+import analyzer.ui.dangling.DanglingTagAnalyzeResultView;
+
+import analyzer.AnalyzerEditorFactory;
+import analyzer.analysis.virtual.VirtualDepend;
+import analyzer.analysis.event.EventDepend;
+import analyzer.ui.linked.ObjectTagLinkAnalyzeResultView;
+import analyzer.ui.virtual.VirtualTagAnalyzeResultView;
 import analyzer.Analyzer;
+import analyzer.constants.AnalyzerConstants;
 
-public class VirtualTagDependAnalyzeAction extends AbstractCommonAction implements PropertyChangeListener {
+/************************************************
+ * @date	: 2020. 5.07.
+ * @책임자 : 박보미
+ * @설명  	: Virtual 태그 종속성 분석 Action 클래스
+ * @변경이력 	: 
+ ************************************************/
 
-	/**
-	 * 
-	 */
+public class VirtualTagDependAnalyzeAction extends AbstractProjectRelatedAction {
+
+
 	private static final long serialVersionUID = 1L;
+	private VirtualTagAnalyzeDialog analyzeDialog;
 
-	public VirtualTagDependAnalyzeAction(Analyzer alayzer) {
-		super(alayzer);
-		setEnabled(true);
-		// TODO Auto-generated constructor stub
+	public VirtualTagDependAnalyzeAction(Analyzer dev) {
+		super(dev);
 	}
 
+	/* (non-Javadoc)
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		System.out.println("VIRTUAL_TAG_DEPENDENCY_ANALYSIS 출력");
-
-	}
-
-	@Override
-	public void propertyChange(PropertyChangeEvent evt) {
-		// TODO Auto-generated method stub
-
+		Analyzer analyzer = getAnalyzer();
+//		if(null == analyzeDialog) {
+			analyzeDialog = new VirtualTagAnalyzeDialog(analyzer);
+//		}
+		analyzeDialog.setLocationRelativeTo(analyzer);
+		//
+		//
+		analyzeDialog.setTagDictionary(analyzer.getProject().getTagDictionary());
+		analyzeDialog.setVisible(true);
+//		
+//		// TODO �м�/�ּ� Ȯ���Ͽ� ��� ǥ��.
+////		AnalyzerEditorFactory.getFactory().getEditor(key);
+//		analyzer.showWorkspace(AnalyzerEditorFactory.VIRTUAL_TAG_DEPENDENCY_RESULT_VIEW_KEY);
+		
+		
+		if(null != analyzeDialog.getSelectedTag()) {
+			VirtualTagAnalyzeResultView view = (VirtualTagAnalyzeResultView) AnalyzerEditorFactory.getFactory().
+					getEditor(AnalyzerEditorFactory.VIRTUAL_TAG_DEPENDENCY_RESULT_VIEW_KEY);
+			EventDepend eventDepend = new EventDepend();
+			eventDepend.analyzer(analyzer.getProject().getEventDictionary());
+			
+			view.setVirtualTagDepends(eventDepend.getVirtualTagEventDepend(analyzeDialog.getSelectedTag()), analyzeDialog.getSelectedTag());
+			/**
+			 * �����ּ� ���Ӽ� ������ Ȯ���Ͽ� ������ ǥ���Ѵ�.
+			 * ���� �ɼ������� �����ϰ�, ������ �м��Ͽ� �����⿡ �ٷ� ǥ���Ѵ�.
+			 */
+			analyzer.showWorkspace(AnalyzerEditorFactory.VIRTUAL_TAG_DEPENDENCY_RESULT_VIEW_KEY);
+		}
+		else {
+//			view.setVirtualTagDepends(new ArrayList<VirtualDepend>(), "");
+		}
+//		String selectedTag = analyzeDialog.getSelectedTag();
+//		List<String> tagList = new ArrayList<String>();
+//		if(null != selectedTag) {
+//			TagDictionary dictionary = analyzer.getProject().getTagDictionary();
+//			for(Tag tag : dictionary.getAllNoneSystemDataTags()) {
+//				if(tag.isHardwareTag()) {
+//					tagList.add(tag.getKey());
+//				}
+//			}
+			// �̺�Ʈ ������ �±׸� �˻��Ͽ� ����� ����Ʈ �𵨷� ������.
+//		}
+		
+		
+		
 	}
 
 }
--- UClair/src/analyzer/action/WindowTestSheetGenerateAction.java
+++ UClair/src/analyzer/action/WindowTestSheetGenerateAction.java
@@ -1,15 +1,36 @@
 package analyzer.action;
 
 import java.awt.event.ActionEvent;
+
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.io.File;
 
 import javax.swing.Action;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.SwingWorker;
 
 import analyzer.Analyzer;
+import com.naru.common.ui.NormalProgressWindow;
+import analyzer.constants.AnalyzerConstants;
+import analyzer.testcase.window.WindowTestSheetGenerator;
+import com.naru.uclair.draw.common.HMIDrawingFilenameFilter;
 
-public class WindowTestSheetGenerateAction extends AbstractCommonAction implements PropertyChangeListener {
+/************************************************
+ * @date	: 2020. 5.07.
+ * @책임자 : 박보미
+ * @설명  	: 화면 테스트 시트 생성(화면 테스트 케이스) Action 클래스
+ * @변경이력 	: 
+ ************************************************/
 
+public class WindowTestSheetGenerateAction extends AbstractProjectRelatedAction implements PropertyChangeListener {
+	
+
+	private static final long serialVersionUID = 1L;
+	private JFileChooser fileChooser;
+	private NormalProgressWindow progressWindow;
+	
 	public WindowTestSheetGenerateAction(Analyzer alayzer) {
 		super(alayzer);
 		setEnabled(true);
@@ -20,6 +41,74 @@ public class WindowTestSheetGenerateAction extends AbstractCommonAction implemen
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		System.out.println("WindowTestSheetGenerateAction");
+		Analyzer analyzer = getAnalyzer();
+		if(null == progressWindow ) {
+			progressWindow = new NormalProgressWindow(analyzer);
+			progressWindow.setModal(true);
+		}
+		progressWindow.setProgress(0);
+		progressWindow.setLocationRelativeTo(analyzer);
+		
+		// 1. ȭ�� ��� ����.
+		File windowResource = new File(getProject().getWindowResourcePath());
+		String[] windowFileNames = windowResource.list(new HMIDrawingFilenameFilter());
+		
+		// 2. ȭ��� ����â ǥ��.
+		// 2-1. ���� ����� ���� ��� ���ٰ� ǥ��.
+		if(null != windowFileNames
+				&& windowFileNames.length > 0) {
+			final String selectWindowName = (String) JOptionPane.showInputDialog(analyzer, 
+					AnalyzerConstants.getString("WindowTestSheetGenerate.InputDialog.Msg"),  //$NON-NLS-1$
+					AnalyzerConstants.getString("WindowTestSheetGenerate.InputDialog.Title"),  //$NON-NLS-1$
+					JOptionPane.PLAIN_MESSAGE, 
+					null, 
+					windowFileNames, windowFileNames[0]);
+			
+			if(null != selectWindowName) {
+				if(null == fileChooser) {
+					fileChooser = new JFileChooser();
+					fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+				}
+				int state = fileChooser.showOpenDialog(analyzer);
+				if(JFileChooser.APPROVE_OPTION == state) {
+					final WindowTestSheetGenerator generator = WindowTestSheetGenerator.getInstance(getProject());
+					generator.addPropertyChangeListeners(this);
+					
+					SwingWorker<String, String> worker = new SwingWorker<String, String>(){
+						
+						@Override
+						protected String doInBackground() throws Exception {
+							File saveDir = fileChooser.getSelectedFile();
+							try {
+								generator.generate(selectWindowName, saveDir);
+								JOptionPane.showMessageDialog(getAnalyzer(), 
+										AnalyzerConstants.getString("WindowTestSheetGenerate.CompleteMsg")); //$NON-NLS-1$
+							} catch (Exception e) {
+								JOptionPane.showMessageDialog(getAnalyzer(), 
+										e.getMessage(), 
+										AnalyzerConstants.getString("WindowTestSheetGenerate.Error.Title"),  //$NON-NLS-1$
+										JOptionPane.ERROR_MESSAGE);
+							}
+							return null;
+						}
+						
+						@Override
+						protected void done() {
+							progressWindow.dispose();
+						}
+					};
+					worker.execute();
+					progressWindow.setVisible(true);
+				}
+			}
+			
+		}
+		else {
+			JOptionPane.showMessageDialog(analyzer, 
+					AnalyzerConstants.getString("WindowTestSheetGenerate.Warning.Msg"),  //$NON-NLS-1$
+					AnalyzerConstants.getString("WindowTestSheetGenerate.Warning.Title"),  //$NON-NLS-1$
+					JOptionPane.WARNING_MESSAGE);
+		}
 	}
 
 	@Override
